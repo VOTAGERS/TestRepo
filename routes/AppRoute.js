8f0d069362d2164a0f9e346abfdadfa954cdab36
GanesYudhaK <ganesyudhakusuma@gmail.com>
2025-06-22 11:09:59 +0700

    feat(UserMenu): Add Index for User List using datatables

--- routes/AppRoute.js
+++ routes/AppRoute.js
@@ -3,12 +3,14 @@ import { AppController } from '../controllers/AppController.js';
 import { AuthMiddleware } from '../controllers/OAuthController.js';
 import passport from '../services/githubService.js';
 const router = express.Router();
+const middleware = AuthMiddleware.ensureAuthenticated
 
 router.get('/', AppController.Index);
 router.post('/join', AppController.Register);
 
 router.get('/session', AuthMiddleware.Login);
-router.get('/workspace', AuthMiddleware.ensureAuthenticated, AuthMiddleware.Dashboard);
+router.get('/workspace', middleware, AuthMiddleware.Dashboard);
+router.get('/users', middleware, AuthMiddleware.Users);
 
 router.get('/auth/github', passport.authenticate('github', { scope: ['user:email'] }));
 
