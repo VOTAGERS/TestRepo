70515669d78873808040e7118b83426843bb28b5
GanesYudhaK <ganesyudhakusuma@gmail.com>
2025-07-20 11:39:04 +0700

    fix deployment

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 FROM node:20-slim
 WORKDIR /app
 COPY package*.json ./
-RUN npm install --production
+RUN npm install --omit=dev
 RUN npm audit fix || (echo "Audit fix failed, continuing..." && true)
 COPY . .
 EXPOSE 8570
--- index.js
+++ index.js
@@ -16,11 +16,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app = express();
 const PORT = process.env.PORT;
-// var morgan = require('morgan');
-
-// Middleware setup
-// app.use(morgan('combined')); // Logging middleware
-
 
 var logger = morgan('combined');
 
@@ -40,8 +35,6 @@ app.use('/', AppRoute);
 
 app.set('view engine', 'ejs');
 app.set('layout', 'layouts/main');
-// app.set('views', path.join(__dirname, 'views'));
-
 
 app.listen(PORT,'0.0.0.0', () => {
     console.log('🔧 Server is starting...');
